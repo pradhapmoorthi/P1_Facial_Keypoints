ca1725584465a46ea1ab5860120b82c778685819
pradhapmoorthi <34316305+pradhapmoorthi@users.noreply.github.com>
2025-12-31 10:58:21 -0500

    Update models.py

--- models.py
+++ models.py
@@ -23,34 +23,6 @@ class SimpleNet(nn.Module):
         x = self.fc1(x)
         return x
 
-class MediumNet(nn.Module):
-    def __init__(self):
-        super(MediumNet, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, 5) # Input 224 -> 220
-        self.pool1 = nn.MaxPool2d(2, 2)  # 220 -> 110. Output: 32x110x110
-        self.dropout1 = nn.Dropout(p=0.1)
-
-        self.conv2 = nn.Conv2d(32, 64, 3) # 110 -> 108
-        self.pool2 = nn.MaxPool2d(2, 2)  # 108 -> 54. Output: 64x54x54
-        self.dropout2 = nn.Dropout(p=0.2)
-
-        self.conv3 = nn.Conv2d(64, 128, 3) # 54 -> 52
-        self.pool3 = nn.MaxPool2d(2, 2)  # 52 -> 26. Output: 128x26x26
-        self.dropout3 = nn.Dropout(p=0.3)
-
-        self.fc1 = nn.Linear(128 * 26 * 26, 512)
-        self.dropout4 = nn.Dropout(p=0.4)
-        self.fc2 = nn.Linear(512, 68 * 2)
-
-    def forward(self, x):
-        x = self.dropout1(self.pool1(F.relu(self.conv1(x))))
-        x = self.dropout2(self.pool2(F.relu(self.conv2(x))))
-        x = self.dropout3(self.pool3(F.relu(self.conv3(x))))
-        x = x.view(x.size(0), -1)
-        x = self.dropout4(F.relu(self.fc1(x)))
-        x = self.fc2(x)
-        return x
-
 class ComplexNet(nn.Module):
     def __init__(self):
         super(ComplexNet, self).__init__()
@@ -204,3 +176,49 @@ class Net(nn.Module):
         # a modified x, having gone through all the layers of your model, should be returned
         return x
 
+class MediumNet(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        
+        ## TODO: Define all the layers of this CNN, the only requirements are:
+        ## 1. This network takes in a square (same width and height), grayscale image as input
+        ## 2. It ends with a linear layer that represents the keypoints
+        ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
+        
+        # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
+        # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
+        self.conv1 = nn.Conv2d(1, 32, 5, stride = 1, padding = 2)
+        self.conv2 = nn.Conv2d(32, 64, 3, stride = 1, padding = 1)
+        self.conv3 = nn.Conv2d(64, 128, 3, stride = 1, padding =1)
+        self.conv4 = nn.Conv2d(128, 256, 3, stride = 1, padding =1)
+        
+        self.FC1 = nn.Linear(14*14*256,2048)
+        self.FC2 = nn.Linear(2048,1024)
+        self.FC3 = nn.Linear(1024,136)
+        
+        self.pool = nn.MaxPool2d(2,2)
+        self.drop1 = nn.Dropout(0.1)
+        self.drop2 = nn.Dropout(0.2)
+        self.drop3 = nn.Dropout(0.3)
+        self.drop4 = nn.Dropout(0.4)
+        self.drop5 = nn.Dropout(0.5)
+        self.drop6 = nn.Dropout(0.6)
+        
+        ## Note that among the layers to add, consider including:
+        # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
+                
+    def forward(self, x):
+        ## TODO: Define the feedforward behavior of this model
+        ## x is the input image and, as an example, here you may choose to include a pool/conv step:
+        x = self.drop1(self.pool(F.elu(self.conv1(x))))
+        x = self.drop2(self.pool(F.elu(self.conv2(x))))
+        x = self.drop3(self.pool(F.elu(self.conv3(x))))
+        x = self.drop4(self.pool(F.elu(self.conv4(x))))
+        x = x.view(x.shape[0],-1)
+        x = self.drop5(F.elu(self.FC1(x)))
+        x = self.drop6(F.elu(self.FC2(x)))
+        x = self.FC3(x)
+                
+        # a modified x, having gone through all the layers of your model, should be returned
+        return x
