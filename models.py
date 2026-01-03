7272fcecd95b54ca471f5d7f074e01c46df8b7d8
pradhapmoorthi <34316305+pradhapmoorthi@users.noreply.github.com>
2026-01-02 19:51:35 -0500

    Update models.py
    Updated Net with new proposed model

--- models.py
+++ models.py
@@ -90,72 +90,53 @@ class Net(nn.Module):
         # Formula: (W-F+2P)/S+ 1. In this case, P=0, therefore the understanding of number of filters/kernels and 
         # the reduced formula, (W-F)/S +1 can be used to compute the dimensions of the output 
         
-        # Conv2D layer 1
-        self.conv1 = nn.Conv2d(1, 32, 5)    ## output tensor shape: (For W:224, F:5, S:1) ... (32, 220, 220)
-        self.pool1 = nn.MaxPool2d(2, 2)     # maxpool that uses a square window of kernel_size=2, stride=2
-        # after maxpooling, output tensor shape: (For W: 220, F:2, S:2, Using formula (W-F)/S +1) ... (32, 110, 110)
+		# Covolutional Layers
+        self.conv_1 = nn.Conv2d(1, 32, 4) 
+        self.conv_2 = nn.Conv2d(32, 64, 3) 
+        self.conv_3 = nn.Conv2d(64, 128, 2) 
+        self.conv_4 = nn.Conv2d(128, 256, 1)
 
-        
-        # Conv2D layer 2
-        self.conv2 = nn.Conv2d(32, 32, 3)   ## output tensor shape: (For W: 110, F: 3, S:1) ... (32, 108, 108)
-        self.pool2 = nn.MaxPool2d(2, 2)     # maxpool that uses a square window of kernel_size=2, stride=2
-        # after maxpooling, output tensor shape: (For W: 108, F:2, S:2) ... (32, 54, 54)
-
-        
-        # Conv2D layer 3
-        self.conv3 = nn.Conv2d(32, 64, 3)   ## output tensor shape: (For W: 54, F: 3, S:1) ... (64, 52, 52)
-        self.pool3 = nn.MaxPool2d(2, 2)     # maxpool that uses a square window of kernel_size=2, stride=2
-        # after maxpooling, output tensor shape: (For W: 52, F:2, S:2) ... (64, 26, 26) 
-
-        
-        # Conv2D layer 4
-        self.conv4 = nn.Conv2d(64, 64, 1)   ## output tensor shape: (For W: 26, F: 1, S:1) ... (64, 26, 26)
-        self.pool4 = nn.MaxPool2d(2, 2)     # maxpool that uses a square window of kernel_size=2, stride=2
-        # after maxpooling, output tensor shape: (For W: 26, F:2, S:2) ... (64, 13, 13)   
-        
-        
-        # Fully-Connected layer 1, fc1
-        self.fc1 = nn.Linear(64*13*13, 4096)
-        # fc1_dropout with p=0.5
-        self.fc1_dropout = nn.Dropout(p=0.5)
-        
-        # Fully-Connected layer 2, fc2
-        self.fc2 = nn.Linear(4096, 1024)
-        # fc2_dropout with p=0.5
-        self.fc2_dropout = nn.Dropout(p=0.5)
-
-        # Fully-Connected layer 3, fc3
-        self.fc3 = nn.Linear(1024, 136)    # Since, 68*2= 136 values, 2 for each of the 68 keypoint (x, y) pairs
-
-        ## Note that among the layers to add, consider including:
-        # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
-        
-
+		# Maxpooling Layer	(for all)	
+        self.pool = nn.MaxPool2d(2, 2)
+		
+		# Dropout (for all)	
+        self.drop_1 = nn.Dropout(0.1)
+        self.drop_2 = nn.Dropout(0.2)
+        self.drop_3 = nn.Dropout(0.3)
+        self.drop_4 = nn.Dropout(0.4)
+        self.drop_5 = nn.Dropout(0.5)
+        self.drop_6 = nn.Dropout(0.6)
+		
+		# Fully Connected Layers (fc)
+        self.fc_1 = nn.Linear(in_features = 43264, out_features = 1000)  #torch.Size([10, 36864]) => in_features:  = 36864
+        self.fc_2 = nn.Linear(in_features = 1000, out_features =  1000)  
+        self.fc_3 = nn.Linear(in_features =  1000, out_features =   136)    #68 keypoints with x and y coordinate => out_features: 136
+		
+		
         
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
-        # All conv2D+ ReLU + Maxpooling layers
-        x = self.pool1(F.relu(self.conv1(x)))
-        x = self.pool2(F.relu(self.conv2(x)))
-        x = self.pool3(F.relu(self.conv3(x)))
-        x = self.pool4(F.relu(self.conv4(x)))
+        ## x = self.pool(F.relu(self.conv1(x)))
         
-        x = x.view(x.size(0), -1)  # Use to Flatten in PyTorch
-        
-        # Fully-Connected Layer 1
-        x = F.relu(self.fc1(x))
-        x = self.fc1_dropout(x)
-        
-        # Fully-Connected Layer 2
-        x = F.relu(self.fc2(x))
-        x = self.fc2_dropout(x)
-        
-        # Fully-Connected Layer 3
-        x = self.fc3(x)
+		
+        x = self.drop_1(self.pool(F.relu(self.conv_1(x))))
+        x = self.drop_2(self.pool(F.relu(self.conv_2(x))))
+        x = self.drop_3(self.pool(F.relu(self.conv_3(x))))
+        x = self.drop_4(self.pool(F.relu(self.conv_4(x))))
+
+		# Flattening the layer
+        x = x.view(x.size(0), -1)
+		
+        # print("in_features size: ", x.size(1))
+
+        x = self.drop_5(F.relu(self.fc_1(x)))
+        x = self.drop_6(F.relu(self.fc_2(x)))
+        x = self.fc_3(x)
         
         # a modified x, having gone through all the layers of your model, should be returned
         return x
+        
 
 class MediumNet(nn.Module):
 
