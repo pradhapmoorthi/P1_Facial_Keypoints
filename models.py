db0509a4b388949b38d6e32a15179ad3b4954e4e
pradhapmoorthi <34316305+pradhapmoorthi@users.noreply.github.com>
2026-01-07 16:41:10 -0500

    Update models.py

--- models.py
+++ models.py
@@ -2,7 +2,7 @@
 import torch
 import torch.nn as nn
 
-class KeypointCNN(nn.Module):
+class Net(nn.Module):
     def __init__(self, n_out=136, in_ch=1):
         super().__init__()
         self.conv1 = nn.Sequential(
